3e339652b0ce79d386c8d2ad64c04c22973bd47e
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2026-04-26 15:28:42 +0200

    Create the 3 classes for part 1 and mainMenu

--- multithreadedAssignment2/Program.cs
+++ multithreadedAssignment2/Program.cs
@@ -2,6 +2,12 @@
 {
     internal class Program
     {
+        public void MainManu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Deadlock");
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
